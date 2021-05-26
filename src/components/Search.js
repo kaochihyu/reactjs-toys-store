ef86a9e650d29153b810b2688b1f8e71fc46c249
kaochihyu <k2307874@gmail.com>
2021-05-27 00:51:41 +0800

    create & update

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { Container } from '../components/Container';
 
 const StyledSearch = styled.div`
   display: flex;
   justify-content: centent;
   align-items: center;
+  width: 14.5rem;
   border-bottom: 0.125rem solid #000;
 `;
 
-export const Search = () => {
+const StyledSearchBar = styled(Container)`
+  position: fixed;
+  top: 80px;
+  z-index: 2;
+  display: flex;
+  justify-content: flex-end;
+  padding-top: ${({ theme }) => theme.space.sm};
+  padding-bottom: ${({ theme }) => theme.space.sm};
+  background-color: #fff;
 
+  ${({ theme }) => theme.media.md} {
+    justify-content: center;
+  }
+`;
+
+export const Search = () => {
   return (
     <StyledSearch>
       <input placeholder="Search..." />
       <AiOutlineSearch size={30} />
     </StyledSearch>
-  )
-}
+  );
+};
+
+export const SearchBar = () => {
+  return (
+    <StyledSearchBar>
+      <Search />
+    </StyledSearchBar>
+  );
+};
