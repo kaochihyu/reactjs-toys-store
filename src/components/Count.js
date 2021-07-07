ce1f453fde27d76f13da4a0283838bb2790ac974
kaochihyu <k2307874@gmail.com>
2021-07-07 22:11:12 +0800

    update

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-// import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
 
 const Operation = styled.button`
   width: 40px;
